876373aeff0f997f8f5d23debdc137a49cab3877
J0shlerB0y <86416124+J0shlerB0y@users.noreply.github.com>
2021-11-13 20:35:15 +0300

    Основная логика
    почти законченный код

--- TestWinForms/TestWinForms/Form1.cs
+++ TestWinForms/TestWinForms/Form1.cs
@@ -26,12 +26,36 @@ namespace TestWinForms
         {
             char[] userInput = new char[1000000000];
             userInput = text_to_trans.Text.ToCharArray();
+            input_text.Text = encryption(userInput).ToString();
         }
-        private char[] encryption()
+        private StringBuilder encryption(char[] input)
         {
             char[] alphabet = new char[] { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' };
-            char[] output = new char[1000000000];
-
+            StringBuilder output = new StringBuilder();
+            for(int i = 0; i < input.Length;i++)
+            {
+                if (input[i] == ' ')
+                {
+                    output.Append(input[i]);
+                }
+                else
+                {
+                    for (int j = 0; j < alphabet.Length; j++)
+                    {
+                        if (input[i] == alphabet[j])
+                        {
+                            if (j + Convert.ToInt32(key_code.Text) <= alphabet.Length)
+                            {
+                                output.Append(alphabet[j + Convert.ToInt32(key_code.Text)]);
+                            }
+                            else
+                            {
+                                output.Append(alphabet[j + Convert.ToInt32(key_code.Text) - alphabet.Length]);
+                            }
+                        }
+                    }
+                }
+            }
             return output;
         }
     }
